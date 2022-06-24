b8615da655d0456dc173a59d8ea400e18d675420
Javier Rosales <cyjav.dev@gmail.com>
2022-06-23 23:57:23 -0500

    Configure hosting database

--- assets/scripts/php/database.php
+++ assets/scripts/php/database.php
@@ -1,14 +1,8 @@
 <?php
-/*
 $host = "localhost";
 $database = "rinopark_rinoparking";
 $user = "rinopark_rinouser";
 $password = "trarpdb10";
-*/
-$host = "localhost";
-$database = "rinoparking";
-$user = "root";
-$password = "";
 
 try {
     $connection = new PDO("mysql:host=$host;dbname=$database", $user, $password);
